1af33ef8b060b951665ae96ecce78909014ccee2
baiyangcao <baiyangcao@msn.cn>
2017-04-17 22:12:05 +0800

    更新notesbuilder.py脚本添加推送功能

--- notesbuilder.py
+++ notesbuilder.py
@@ -4,6 +4,7 @@ import scrapy
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from os import path
 from urllib.parse import urljoin
+from git import Repo
 
 
 class NotesSpider(scrapy.Spider):
@@ -12,6 +13,9 @@ class NotesSpider(scrapy.Spider):
         'https://baiyangcao.github.io/'
         # 'http://localhost:4000/'
     ]
+    repo_path = r'E:\Notes'
+    readme_path = None
+    index_path = None
     data = []
 
     def __init__(self, **kwargs):
@@ -38,28 +42,52 @@ class NotesSpider(scrapy.Spider):
     def close(self, reason):
         self.generate_readme()
         self.generate_index()
+        self.push_repo()
 
     def generate_readme(self):
+        '''
+        generate readme.md
+        :return: 
+        '''
         try:
             readme_tempalte = self.env.get_template('README.md')
             readme = readme_tempalte.render(notes=self.data).encode('utf-8')
-            readme_name = getattr(self, 'readme', 'README.md')
-            with open(readme_name, 'wb') as file:
+            self.readme_path = getattr(self, 'readme', 'README.md')
+            with open(self.readme_path, 'wb') as file:
                 file.write(readme)
                 print('generate README.md successfully')
         except Exception as ex:
             print('generate README.md error: ', ex)
 
     def generate_index(self):
+        '''
+        generate docs/index.html 
+        :return: 
+        '''
         try:
             index_template = self.env.get_template('index.html')
             index = index_template.render(notes=self.data).encode('utf-8')
             index_name = getattr(self, 'index', 'index.html')
-            index_path = path.join('docs', index_name)
-            with open(index_path, 'wb') as file:
+            self.index_path = path.join('docs', index_name)
+            with open(self.index_path, 'wb') as file:
                 file.write(index)
                 print('generate index.html successfully')
         except Exception as ex:
             print('generate index.html error: ', ex)
 
+    def push_repo(self):
+        '''
+        add readme.md, index.html file to stage, commit, push to remote
+        :return: 
+        '''
+        repo = Repo(self.repo_path)
+        index = repo.index
 
+        # commit
+        index.add(self.readme_path)
+        index.add(self.index_path)
+        index.commit('update readme.md and index.html automatically')
+
+        # push
+        remote = repo.remote()
+        remote.push()
